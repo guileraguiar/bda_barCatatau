a6121bea2870c214c7b327497786a1744e7a708c
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2021-09-07 22:16:54 -0300

    POPULANDO TABELAS

--- criacao_tabela.sql
+++ criacao_tabela.sql
@@ -3,7 +3,7 @@ create database bda_catatau
 use bda_catatau
 
 create TABLE clientes (
-    cod_cliente int identity(00, 1) CONSTRAINT PK_CLIENTE primary key,
+    cod_cliente int identity(1, 1) CONSTRAINT PK_CLIENTE primary key,
     nome VARCHAR (255),
     cpf VARCHAR (20),
     sexo varchar (20),
@@ -12,7 +12,7 @@ create TABLE clientes (
 )
 
 create TABLE fornecedores (
-    cod_fornecedor int identity(00, 1) CONSTRAINT PK_FORNECEDOR primary key,
+    cod_fornecedor int identity(1, 1) CONSTRAINT PK_FORNECEDOR primary key,
     razao_social VARCHAR (255),
     cnpj VARCHAR (20),
     endereco varchar (300),
@@ -20,7 +20,7 @@ create TABLE fornecedores (
 )
 
 create TABLE funcionarios (
-    cod_funcionario int identity(00, 1) CONSTRAINT PK_FUNCIONARIO primary key,
+    cod_funcionario int identity(1, 1) CONSTRAINT PK_FUNCIONARIO primary key,
     nome VARCHAR (255),
     cpf VARCHAR (20),
     sexo varchar (20),
@@ -30,11 +30,13 @@ create TABLE funcionarios (
 
 create TABLE mercadoria (
     cod_produto int identity(00, 1) CONSTRAINT PK_PRODUTO primary key,
+    cod_fornecedor int,
     nome_produto VARCHAR (255),
     descricao text,
     qtde_estoque int,
     preco_unit float,
     dt_cadastro date,
+    CONSTRAINT cod_fornecedor FOREIGN KEY (cod_fornecedor) REFERENCES fornecedores(cod_fornecedor),
 )
 
 create TABLE comanda (
@@ -48,6 +50,12 @@ create TABLE comanda (
     CONSTRAINT cod_produto FOREIGN KEY (cod_produto) REFERENCES mercadoria(cod_produto),
 )
 
+SELECT * from comanda
+
+ALTER TABLE dbo.comanda DROP COLUMN dt_cadastro;
+ALTER TABLE comanda ADD sub_total float;
+exec sp_rename 'Comanda.[qtde_estoque]', 'qtde', 'column'
+
 create TABLE cupom_fiscal (
     coo int identity(000, 1) CONSTRAINT PK_COO primary key,
     num_comanda int,
@@ -56,3 +64,5 @@ create TABLE cupom_fiscal (
     nome_produto varchar(255),
     CONSTRAINT num_comanda FOREIGN KEY (num_comanda) REFERENCES comanda(num_comanda)
 )
+
+SELECT * FROM cupom_fiscal
