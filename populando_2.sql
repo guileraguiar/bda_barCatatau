4d1094d4f6fba2c77d4c530773e2f3adeed4bceb
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2021-09-21 20:15:37 -0300

    inserção do script de inserção da empresa

--- populando_2.sql
+++ populando_2.sql
@@ -11,6 +11,8 @@ VALUES ('WHISKY - JOHNNIE WALKER - GARRAFA - 1L - RED LABEL',108.49)
 INSERT item (nome_item , preco)
 VALUES ('DESTILADO - STEINHAEGER - GARRAFA - 900ML - DOBLE W',79.99)
 
+SELECT * FROM item
+
 INSERT Funcionario (nome , endereco, cpf, cnpj)
 VALUES ('Leandro Jorge Oliver Melo', 'Servidão Carlos Welter - N°188 – Bairro: Bom Retiro – Cidade: Joinville - SC', '578.647.199-37', '02.564.742/0001-52')
 INSERT Funcionario (nome , endereco, cpf, cnpj)
@@ -21,3 +23,10 @@ INSERT Funcionario (nome , endereco, cpf, cnpj)
 VALUES ('Caleb Rodrigo Leonardo Bernardes', 'Rua Expedicionário Erwin Kricheldorf - N°362 – Bairro: Vila Nova – Cidade: Joinville - SC', '215.946.679-42', '02.564.742/0001-52')
 INSERT Funcionario (nome , endereco, cpf, cnpj)
 VALUES ('Carlos Eduardo Miguel Augusto Oliveira', 'Servidão Oscar Kasulke - N°223 – Bairro: Nova Brasília – Cidade: Joinville - SC', '124.542.671-90', '02.564.742/0001-52')
+
+SELECT * FROM Funcionario
+
+INSERT Bar_do_Catatau (razao_social, endereco, cnpj, dt_abertura)
+VALUES ('Bar do Catatau LTDA', 'Servidão Carlos Welter - N°188 – Bairro: Bom Retiro – Cidade: Joinville - SC', '02.564.742/0001-52', '2021-02-15')
+
+SELECT * FROM Bar_do_Catatau
