d28419c94c92c841335ff8aa9d24b4a1457a2630
guileraguiar <54964014+guileraguiar@users.noreply.github.com>
2021-09-21 21:47:09 -0300

    inserção da proc de cadastrar comanda

---
+++ proc_cadastraComanda.sql
@@ -0,0 +1,24 @@
+CREATE PROCEDURE cadastrarComanda (
+    @num_comanda int, @nome_item char(255), @preco float, @qtde_item int, @preco_total float, @dt_emissao date
+)
+AS
+BEGIN
+
+INSERT INTO Comanda (nome_item, preco, qtde_item, dt_emissao)
+VALUES (
+    @nome_item, @preco, @qtde_item, @dt_emissao
+)
+
+UPDATE Comanda SET preco_total = preco * qtde_item WHERE num_comanda = @num_comanda
+END
+
+EXEC cadastrarComanda
+@num_comanda = 2,
+@nome_item = 'WHISKY - JOHNNIE WALKER - GARRAFA - 1L - RED LABEL',
+@preco = 108.49,
+@qtde_item = 4,
+@dt_emissao = '2021-07-27',
+@preco_total = ''
+
+SELECT * from Comanda
+SELECT * from item
